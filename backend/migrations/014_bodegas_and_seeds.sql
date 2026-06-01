73ca4ffd63373c34e05a311d00e4733127023d51
n0cod3develper-byte <n0cod3develper@gmail.com>
2026-06-01 12:28:48 -0500

    Cambio en tablas Ubicacion Bodega para ejecutar la migracion

--- backend/migrations/014_bodegas_and_seeds.sql
+++ backend/migrations/014_bodegas_and_seeds.sql
@@ -70,4 +70,43 @@ WHERE tipo = 'PRODUCTO' AND ubicacion_id IS NULL;
 ALTER TABLE inventory_items
   DROP COLUMN IF EXISTS ubicacion_bodega;
 
+-- 4. Recrear vista catalogo_completo incluyendo la información de ubicación
+DROP VIEW IF EXISTS catalogo_completo CASCADE;
+
+CREATE VIEW catalogo_completo AS
+SELECT
+  i.id,
+  i.tipo,
+  i.codigo_interno,
+  i.name AS nombre_interno,
+  i.nombre_comercial,
+  i.sku AS referencia_sistema,
+  i.referencia_fabricante,
+  c.nombre AS categoria_nombre,
+  c.color_hex AS categoria_color,
+  u.abreviatura AS unidad_medida,
+  i.stock_current AS stock_actual,
+  i.stock_minimum AS stock_minimo,
+  -- Ubicación
+  ub.codigo_ubicacion,
+  ub.bodega as bodega_nombre,
+  -- Precios unificados
+  CASE 
+    WHEN i.tipo = 'PRODUCTO' THEN i.unit_price 
+    ELSE i.precio_servicio 
+  END AS precio_venta,
+  CASE 
+    WHEN i.tipo = 'PRODUCTO' THEN i.unit_cost 
+    ELSE i.precio_servicio_minimo 
+  END AS costo_o_minimo,
+  i.aplica_iva,
+  i.iva_pct,
+  i.is_active,
+  i.activo_catalogo,
+  i.imagen_thumb_url
+FROM inventory_items i
+LEFT JOIN catalogo_categorias c ON c.id = i.categoria_id
+LEFT JOIN unidades_medida u ON u.id = i.unidad_medida_id
+LEFT JOIN ubicaciones_bodega ub ON ub.id = i.ubicacion_id;
+
 COMMIT;
